1ea832380589370dda1f1110fac7be47add8ff36
Amit Kumar <amit7646014@gmail.com>
2022-12-20 22:31:38 +0530

    updated working code

--- src/main/java/com/skyamit/covidtracker/models/LocationStats.java
+++ src/main/java/com/skyamit/covidtracker/models/LocationStats.java
@@ -4,44 +4,58 @@ public class LocationStats {
 
     private String state;
     private String country;
-    private String latestTotalCases;
+    private String Death;
+    private String confirmed;
+    private String recovered;
+    private String active;
 
-    public LocationStats(String state, String country, String latestTotalCases) {
+    LocationStats(){}
+
+    public LocationStats(String state, String country, String Death, String confirmed, String recovered,
+            String active) {
         this.state = state;
         this.country = country;
-        this.latestTotalCases = latestTotalCases;
+        this.Death = Death;
+        this.confirmed = confirmed;
+        this.recovered = recovered;
+        this.active = active;
     }
-
     public String getState() {
         return state;
     }
-
     public void setState(String state) {
         this.state = state;
     }
-
     public String getCountry() {
         return country;
     }
-
     public void setCountry(String country) {
         this.country = country;
     }
-
-    public String getLatestTotalCases() {
-        return latestTotalCases;
+    public String getDeath() {
+        return Death;
+    }
+    public void setDeath(String Death) {
+        this.Death = Death;
+    }
+    public String getConfirmed() {
+        return confirmed;
+    }
+    public void setConfirmed(String confirmed) {
+        this.confirmed = confirmed;
+    }
+    public String getRecovered() {
+        return recovered;
+    }
+    public void setRecovered(String recovered) {
+        this.recovered = recovered;
+    }
+    public String getActive() {
+        return active;
+    }
+    public void setActive(String active) {
+        this.active = active;
     }
 
-    public void setLatestTotalCases(String latestTotalCases) {
-        this.latestTotalCases = latestTotalCases;
-    }
 
-    @Override
-    public String toString() {
-        return "LocationStats{" +
-                "state='" + state + '\'' +
-                ", country='" + country + '\'' +
-                ", latestTotalCases=" + latestTotalCases +
-                '}';
-    }
 }
